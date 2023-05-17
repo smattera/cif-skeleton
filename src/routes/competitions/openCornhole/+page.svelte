c111b425a4047f2751d199eff8143fec6220c2e0
Sonny Mattera <smattera@hotmail.com>
2023-05-17 15:20:00 -0500

    Update Cornhole

--- src/routes/competitions/openCornhole/+page.svelte
+++ src/routes/competitions/openCornhole/+page.svelte
@@ -9,7 +9,7 @@
 <svelte:head>
   <meta property="og:title" content="Open Cornhole Tournament" />
   <meta property="og:description" content="Saturday, July 15, 2023 ⬦ Starts at 10:00 AM ⬦ Free Fair Admission With Signup" />
-  <meta property="og:image" content="https://cif-skeleton.vercel.app/meta/openCornhole.jpeg" />
+  <meta property="og:image" content="https://cif-skeleton.vercel.app/img/meta/openCornhole.jpeg" />
   <meta property="og:url" content="https://www.choctawindianfair.com/competitions/openCornhole" />
   <meta property="og:type" content="website" />
 </svelte:head>
