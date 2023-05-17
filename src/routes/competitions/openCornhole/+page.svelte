5aa65c9a100a54c359e29981b4e9698b6ad1bb2e
Sonny Mattera <smattera@hotmail.com>
2023-05-17 15:01:44 -0500

    Update Cornhole

---
+++ src/routes/competitions/openCornhole/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { onMount } from 'svelte';
+  
+  onMount(() => {
+    window.location.href = 'https://forms.office.com/r/dre886jmaJ'; // Replace with your desired redirect URL
+  });
+</script>
+
+<svelte:head>
+  <meta property="og:title" content="Open Cornhole Tournament" />
+  <meta property="og:description" content="Saturday, July 15, 2023 ⬦ Starts at 10:00 AM ⬦ Free Fair Admission With Signup" />
+  <meta property="og:image" content="https://www.choctawindianfair.com/assets/img/meta/openCornhole.jpeg" />
+  <meta property="og:url" content="https://www.choctawindianfair.com/competitions/openCornhole.html" />
+  <meta property="og:type" content="website" />
+</svelte:head>
+
+<p>Redirecting...</p>
---
+++ src/routes/competitions/tribalCornhole/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { onMount } from 'svelte';
+  
+  onMount(() => {
+    window.location.href = 'https://forms.office.com/r/GmU5rVbqA0'; // Replace with your desired redirect URL
+  });
+</script>
+
+<svelte:head>
+  <meta property="og:title" content="Open Cornhole Tournament" />
+  <meta property="og:description" content="Friday, July 14, 2023 ⬦ Starts at 10:00 AM ⬦ Open to Tribal Affiliates" />
+  <meta property="og:image" content="https://www.choctawindianfair.com/assets/img/meta/tribalCornhole.jpeg" />
+  <meta property="og:url" content="https://www.choctawindianfair.com/competitions/tribalCornhole.html" />
+  <meta property="og:type" content="website" />
+</svelte:head>
+
+<p>Redirecting...</p>
